f3f3e373096037c797567db4e74fb8c94f2b0ef7
sashgorokhov <sashgorokhov@gmail.com>
2016-09-13 13:38:44 +0300

    Fixes and updates

--- puppet/modules/certs/manifests/ca.pp
+++ puppet/modules/certs/manifests/ca.pp
@@ -4,6 +4,9 @@ class certs::ca inherits certs::params {
   file {$ssl_dir:
    ensure => directory
   }->
+  file {'/etc/kubernetes/shared':
+   ensure => directory
+  }->
   exec {'ca-key.pem':
     command => "/usr/bin/openssl genrsa -out /etc/kubernetes/shared/ssl/ca-key.pem 2048",
     unless => "/usr/bin/test -f /etc/kubernetes/shared/ssl/ca-key.pem"
